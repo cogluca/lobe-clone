de59ddc4878342cbf5ab16d570db548b6faa03dd
Gianluca Cognigni <cog.gianluca@gmail.com>
2022-08-01 03:26:06 +0200

    styled navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,4 @@
-import { Container, Nav, Navbar, NavLink} from 'react-bootstrap'
-
+import {Button, Container, Nav, Navbar, NavLink} from 'react-bootstrap'
 
 
 const LobeNavbar = (props) => {
@@ -7,17 +6,30 @@ const LobeNavbar = (props) => {
     return (
 
         <Navbar bg="light">
-            <Container>
+            <Container className="header justify-content-around flex-row flex-grow-1 ">
                 <Navbar.Brand href="#home">
-                    <img src="/static_content/Lobo.png" alt={"Lobo logo"} style={{width: "5%", height: "auto"}}/>
+                    <img src="/static_content/Lobo.png" alt={"Lobo logo. Click for home"} style={{maxWidth: "5vw", height: "auto", width: "100%"}}/>
                 </Navbar.Brand>
-                <Nav className="me-auto">
+                <Nav className="flex" style={{
+                    display: "flex",
+                    flexFlow: "row",
+                    justifyContent: "space-around",
+                    alignItems: "center",
+                    verticalAlign: "middle"
+                }}>
                     <NavLink href="#overview"> Overview </NavLink>
                     <NavLink href="#examples"> Examples </NavLink>
                     <NavLink href="tour"> Tour </NavLink>
                     <NavLink href="blog"> Blog </NavLink>
                     <NavLink href="#help"> Help </NavLink>
                 </Nav>
+                <Button style={{
+                    background: "\n" +
+                        "#04ddb2", outlineColor: "\n" +
+                        "#04ddb2"
+                }}>
+                    Download
+                </Button>
             </Container>
 
 
